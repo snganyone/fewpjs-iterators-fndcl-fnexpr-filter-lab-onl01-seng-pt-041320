edc1f657bf7616aea5936af0de298271ff071d96
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-11-11 00:01:33 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(arr, str) {
+    return arr.filter(function(word){
+        return word.toLowerCase().indexOf(str.toLowerCase()) >= 0;
+    });
+}
+
+function fuzzyName(arr, str){
+    return arr.filter();
+}
